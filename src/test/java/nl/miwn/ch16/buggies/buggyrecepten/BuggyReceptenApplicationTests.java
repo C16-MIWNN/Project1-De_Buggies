cb62f0e07e1899e54a93a891bfe09757950e4778
joostnuman <joost.numan1@gmail.com>
2025-06-23 15:56:55 +0200

    Project created through springboot starter

---
+++ src/test/java/nl/miwn/ch16/buggies/buggyrecepten/BuggyReceptenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwn.ch16.buggies.buggyrecepten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuggyReceptenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
