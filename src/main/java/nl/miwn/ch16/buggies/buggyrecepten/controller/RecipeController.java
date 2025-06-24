26cc842f8d947d6d4041e01b1dc11ddebf78a15e
joostnuman <joost.numan1@gmail.com>
2025-06-24 11:24:30 +0200

    added application properties for a connection with the DataBase

--- src/main/java/nl/miwn/ch16/buggies/buggyrecepten/controller/RecipeController.java
+++ src/main/java/nl/miwn/ch16/buggies/buggyrecepten/controller/RecipeController.java
@@ -2,7 +2,27 @@ package nl.miwn.ch16.buggies.buggyrecepten.controller;
 
 /*
  * @Author: Joost Numan
- * zet hier wat het programma doet
+ * this program controls all actions regarding the recipe class
  */
+
+import nl.miwn.ch16.buggies.buggyrecepten.repositories.IngredientRepository;
+import nl.miwn.ch16.buggies.buggyrecepten.repositories.RecipeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RecipeController {
+    private final RecipeRepository recipeRepository;
+    private final IngredientRepository ingredientRepository;
+
+    public RecipeController(RecipeRepository recipeRepository, IngredientRepository ingredientRepository) {
+        this.recipeRepository = recipeRepository;
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping("/")
+    private String showHomePage(Model datamodel) {
+        return "homePage";
+    }
 }
