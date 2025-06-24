0687019738b04dfc4b2ba97de632369321ba492a
Marnix Ripke <191800038+MarnixRipke@users.noreply.github.com>
2025-06-24 13:50:01 +0200

    WIP making recipe details

--- src/main/java/nl/miwn/ch16/buggies/buggyrecepten/controller/RecipeController.java
+++ src/main/java/nl/miwn/ch16/buggies/buggyrecepten/controller/RecipeController.java
@@ -5,13 +5,20 @@ package nl.miwn.ch16.buggies.buggyrecepten.controller;
  * this program controls all actions regarding the recipe class
  */
 
+import nl.miwn.ch16.buggies.buggyrecepten.model.Recipe;
 import nl.miwn.ch16.buggies.buggyrecepten.repositories.IngredientRepository;
 import nl.miwn.ch16.buggies.buggyrecepten.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import java.util.List;
 
 @Controller
+
 public class RecipeController {
     private final RecipeRepository recipeRepository;
     private final IngredientRepository ingredientRepository;
@@ -21,8 +28,25 @@ public class RecipeController {
         this.ingredientRepository = ingredientRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping({"/", "/homePage"})
     private String showHomePage(Model datamodel) {
+        List<Recipe> allRecipes = recipeRepository.findAll();
+
+
+
+        datamodel.addAttribute("allRecipes", allRecipes);
+
         return "homePage";
     }
+
+    @GetMapping("/recipeDetails/{recipeId}")
+    private String showRecipeDetails(@PathVariable ("recipeId") Long recipeId, Model datamodel)  {
+
+
+
+        return "recipeDetails";
+    }
+
+
 }
+
